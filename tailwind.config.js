04c8fb533a3449374fce1e6d45464622acc43a19
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2022-07-21 00:28:35 +0300

    Done with tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
+
+    //adding Poppins font as a custom font
+    fontFamily: {
+      'poppins': 'Poppins, sans-serif'
+    },
+
+
+    // Adding custom colors
+    colors: {
+      'onPrimary': '#000000',
+      'onPrimaryLight': '#1F1F1F'
+    },
+
+
     extend: {},
   },
   plugins: [
